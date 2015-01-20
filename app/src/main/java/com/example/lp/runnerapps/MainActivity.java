2dc2eb5a299c8f7ba771102322149a00ff05ed09
Goadah <goadah@gmail.com>
2015-01-20 11:43:53 -0800

    Added change screen function

--- app/src/main/java/com/example/lp/runnerapps/MainActivity.java
+++ app/src/main/java/com/example/lp/runnerapps/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.lp.runnerapps;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+
+    private void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.end_button_bottom);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, ReturnPage.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -35,7 +59,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
-
 }
