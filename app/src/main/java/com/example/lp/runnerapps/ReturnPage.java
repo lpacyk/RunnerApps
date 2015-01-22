b5f39b9b053ecc82c085e541eeefabebbdfa157c
Goadah <goadah@gmail.com>
2015-01-21 17:34:17 -0800

    Wednesday Deliverable Accomplished!

--- app/src/main/java/com/example/lp/runnerapps/ReturnPage.java
+++ app/src/main/java/com/example/lp/runnerapps/ReturnPage.java
@@ -1,16 +1,39 @@
 package com.example.lp.runnerapps;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class ReturnPage extends ActionBarActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_return_page);
+        addListenerOnButton();
+    }
+
+    private void addListenerOnButton() {
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, StartRun.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
