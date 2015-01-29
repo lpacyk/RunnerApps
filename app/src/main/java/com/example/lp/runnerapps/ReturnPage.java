dd08fb86c00de3f32f82a01016ac4cad29e37d91
Logan <logan.hollins1@gmail.com>
2015-01-28 21:30:32 -0800

    Added Timer to return screen and fucked up interface even more

--- app/src/main/java/com/example/lp/runnerapps/ReturnPage.java
+++ app/src/main/java/com/example/lp/runnerapps/ReturnPage.java
@@ -2,15 +2,22 @@ package com.example.lp.runnerapps;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
 public class ReturnPage extends ActionBarActivity {
-
+    private TextView timerValue;
+    private long startTime = 0L;
+    private Handler customHandler = new Handler();
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
     Button button;
 
     @Override
@@ -18,7 +25,42 @@ public class ReturnPage extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_return_page);
         addListenerOnButton();
+
+        timerValue = (TextView) findViewById(R.id.textView4);
+        timerValue.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimerThread, 0);
+
+            }
+        });
+
+
+
     }
+    private Runnable updateTimerThread = new Runnable() {
+
+        public void run() {
+
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) (updatedTime % 1000);
+            timerValue.setText("" + mins + ":"
+                    + String.format("%02d", secs) + ":"
+                    + String.format("%03d", milliseconds));
+            customHandler.postDelayed(this, 0);
+        }
+
+    };
+
+
+
 
     private void addListenerOnButton() {
         final Context context = this;
@@ -63,4 +105,5 @@ public class ReturnPage extends ActionBarActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
 }
